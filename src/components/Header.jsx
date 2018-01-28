fd1d1f37fc9681623eb25981323729b6c805a601
twishasaraiya <saraiyatwisha@gmail.com>
2018-01-28 11:50:30 +0530

    updated sidebar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 class Header extends React.Component{
   render(){
     return(
-      <div className="header"><h1>Header</h1></div>
+      <div className="header">
+        <h2 className='logo'>Logo Here</h2>
+        <input className="textBox" type="text"></input>
+        <p className='lorem'>Lorem</p>
+        <p className='lorem'>Lorem</p>
+          <img className='icons' src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Plain_Disc_20%25_grey.svg/2000px-Plain_Disc_20%25_grey.svg.png"></img>
+          <img className='icons' src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Plain_Disc_20%25_grey.svg/2000px-Plain_Disc_20%25_grey.svg.png"></img>
+          <img className='icons' src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Plain_Disc_20%25_grey.svg/2000px-Plain_Disc_20%25_grey.svg.png"></img>
+
+          <div className=''>
+            <img className='icons' src="http://glia.ca/scm/2013/ge1127_b/wp-content/uploads/2013/10/594partner-profile-pic-An.jpg"></img>
+            <img className='dropdownbtn' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Ic_arrow_drop_down_48px.svg/2000px-Ic_arrow_drop_down_48px.svg.png"></img>
+        </div>
+      </div>
     );
   }
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,45 @@ import React from 'react';
 class Sidebar extends React.Component{
   render(){
     return(
-      <div className="sidebar">Sidebar</div>
+      <div className="sidebar">
+        <ul>
+     		<li><a href="#"><span>Home</span></a>
+              <ul>
+           			<li><a href="#">Add</a></li>
+           			<li><a href="#">Edit</a></li>
+           			<li><a href="#">Delete</a></li>
+     		      </ul>
+     	   </li>
+     		<li><a href="#">Blog</a>
+     		  <ul>
+       			<li><a href="#">1</a></li>
+       			<li><a href="#">2</a></li>
+       			<li><a href="#">3</a></li>
+     		  </ul>
+     	</li>
+     		<li><a href="#">Service</a>
+          <ul>
+     		    <li><a href="#">a</a></li>
+       			<li><a href="#">b</a></li>
+       			<li><a href="#">c</a></li>
+     		  </ul>
+     	</li>
+     		<li><a href="#">Porfolio</a>
+          <ul>
+       			<li><a href="#">d</a></li>
+       			<li><a href="#">e</a></li>
+       			<li><a href="#">f</a></li>
+     		</ul>
+     	</li>
+     		<li><a href="#">Contact</a>
+          <ul>
+       			<li><a href="#">Add</a></li>
+       			<li><a href="#">Edit</a></li>
+       			<li><a href="#">Delete</a></li>
+     		   </ul>
+     		</li>
+ 	    </ul>
+      </div>
     );
   }
 }
